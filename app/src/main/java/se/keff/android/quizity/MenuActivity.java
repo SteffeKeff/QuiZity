37ba420c80c3894bd60cc0ae004028e6d280f98e
Maximilian Kern <maxkernw@gmail.com>
2015-03-29 20:36:18 +0200

    added

--- app/src/main/java/se/keff/android/quizity/MenuActivity.java
+++ app/src/main/java/se/keff/android/quizity/MenuActivity.java
@@ -1,9 +1,14 @@
 package se.keff.android.quizity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 
 public final class MenuActivity extends ActionBarActivity {
 
@@ -21,7 +26,29 @@ public final class MenuActivity extends ActionBarActivity {
 
     public void playGame(View view)
     {
+        final Animation animAlpha = AnimationUtils.loadAnimation(this,R.anim.anim_alpha);
+        final Button button = (Button) findViewById(R.id.buttonPlay);
+        view.startAnimation(animAlpha);
         Intent intent = new Intent(this, GameActivity.class);
         startActivity(intent);
     }
+    public void animButton(View view){
+        final Animation animAlpha = AnimationUtils.loadAnimation(this,R.anim.anim_alpha);
+        final Button button = (Button) findViewById(R.id.buttonHighscore);
+        view.startAnimation(animAlpha);
+    }
+    public void animButton2(View view){
+        final Animation animAlpha = AnimationUtils.loadAnimation(this,R.anim.anim_alpha);
+        final Button button = (Button) findViewById(R.id.buttonSettings);
+        view.startAnimation(animAlpha);
+
+    }
+    public void animButton3(View view){
+        final Animation animAlpha = AnimationUtils.loadAnimation(this,R.anim.anim_alpha);
+        final Button button = (Button) findViewById(R.id.buttonHowTo);
+        view.startAnimation(animAlpha);
+
+    }
+
+
 }
