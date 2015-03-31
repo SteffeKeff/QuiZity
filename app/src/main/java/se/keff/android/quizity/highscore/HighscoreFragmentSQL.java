2cdecd3af49badce29caa44f589cf96d96e0019c
Stefan De Geer <sdgkeff@gmail.com>
2015-03-31 21:29:40 +0200

    import

--- app/src/main/java/se/keff/android/quizity/highscore/HighscoreFragmentSQL.java
+++ app/src/main/java/se/keff/android/quizity/highscore/HighscoreFragmentSQL.java
@@ -1,16 +1,12 @@
 package se.keff.android.quizity.highscore;
 
-import android.content.Context;
-import android.content.SharedPreferences;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
@@ -19,15 +15,11 @@ import com.koushikdutta.async.future.FutureCallback;
 import com.koushikdutta.ion.Ion;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.HashMap;
-import java.util.Map;
-import java.util.TreeMap;
 
 import se.keff.android.quizity.R;
 import se.keff.android.quizity.game.Game;
 import se.keff.android.quizity.game.GameActivity;
-import se.keff.android.quizity.game.GameplayFragment;
 
 public final class HighscoreFragmentSQL extends Fragment {
 
